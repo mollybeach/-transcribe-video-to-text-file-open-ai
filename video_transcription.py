e227d0561a62c1464ce6b35a915cffff266dca9d
Molly Beach <mollyjbeach@gmail.com>
2023-04-07 15:53:09 -0700

    Update video_transcription.py

--- video_transcription.py
+++ video_transcription.py
@@ -1,8 +1,21 @@
 import openai
-
 # Set up OpenAI API key
 openai.api_key = "YOUR_API_KEY"
 
-audio_file= open("example_video.mp4", "rb")
+# Set Paths
+audio_path = "example_video.mp4"
+output_path = "output.txt"
+
+# Upload the audio file to OpenAI's transcriptions API
+audio_file= open(audio_path, "rb")
+
+# Transcribe File using OpenAI Whisper
 transcript = openai.Audio.transcribe("whisper-1", audio_file)
-print(transcript)
+
+# Check if the transcript contains the 'text' key
+if "text" in transcript:
+    # Write the transcription to a text file:
+    open(output_path, "w").write(transcript["text"])
+    print("Transcription written to output.txt")
+else :
+    print("No transcription found")
