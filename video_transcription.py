004514abd631efdf61f218f2c1aa2dac62029445
Molly Beach <mollyjbeach@gmail.com>
2023-04-07 16:23:15 -0700

    transcribing larger files 1 min at a time
    transcribing larger files 1 min at a time

--- video_transcription.py
+++ video_transcription.py
@@ -1,22 +1,65 @@
 import openai
+import os
+import math
+from pydub import AudioSegment
+
+AudioSegment.ffmpeg = "/usr/local/bin/ffmpeg"
+AudioSegment.ffprobe = "/usr/local/bin/ffprobe"
 
 # Set up OpenAI API key
-openai.api_key = "YOUR_API_KEY"
+openai.api_key = "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"
 
 # Set Paths
 audio_path = "example_video.mp4"
+audio_path2 = "part2ccla.mp4"
 output_path = "output.txt"
 
-# Upload the audio file to OpenAI's transcriptions API
-audio_file= open(audio_path, "rb")
+# Set the maximum duration for each transcription chunk (in seconds)
+max_chunk_duration = 60
 
-# Transcribe File using OpenAI Whisper
-transcript = openai.Audio.transcribe("whisper-1", audio_file)
+# Load the audio file using pydub
+audio = AudioSegment.from_file(audio_path2, format="mp4")
 
-# Check if the transcript contains the 'text' key
-if "text" in transcript:
-    # Write the transcription to a text file:
-    open(output_path, "w").write(transcript["text"])
-    print("Transcription written to output.txt")
-else :
-    print("No transcription found")
+# Calculate the total number of chunks needed
+num_chunks = math.ceil(len(audio) / (max_chunk_duration * 1000))
+
+# Initialize an empty list to store the transcriptions
+transcriptions = []
+
+# Transcribe each chunk of the audio file
+for i in range(num_chunks):
+    # Calculate the start and end times for the chunk
+    start_time = i * max_chunk_duration * 1000
+    end_time = (i + 1) * max_chunk_duration * 1000
+
+    # Extract the chunk from the audio file
+    chunk = audio[start_time:end_time]
+
+    # Export the chunk to a temporary WAV file
+    chunk.export("temp.wav", format="wav")
+
+    # Upload the temporary WAV file to OpenAI's transcriptions API
+    with open("temp.wav", "rb") as file:
+        response = openai.Audio.transcribe("whisper-1", file)
+    
+    # Check if the response contains the 'text' key
+    if "text" in response:
+        
+        # Append the transcription to the list of transcriptions
+        transcriptions.append(response["text"])
+         # Write the transcription to the output text file:
+        open(output_path, "a").write(response["text"])
+        print("Transcription written to output.txt")
+    else:
+        # If there was an error, append an empty string to the list of transcriptions
+        transcriptions.append("")
+
+    # Delete the temporary WAV file
+    os.remove("temp.wav")
+
+# Write the transcriptions to the output file
+with open(output_path, "w") as file:
+    for transcription in transcriptions:
+        file.write(transcription + "\n")
+
+print("Transcription complete!")
