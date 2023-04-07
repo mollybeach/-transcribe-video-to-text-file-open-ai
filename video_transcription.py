e43ffd93ba2c7471a63a1429357007a1a75b32c0
Molly Beach <mollyjbeach@gmail.com>
2023-04-07 15:30:31 -0700

    Update video_transcription.py

--- video_transcription.py
+++ video_transcription.py
@@ -1,5 +1,8 @@
 import openai
 
+# Set up OpenAI API key
+openai.api_key = "YOUR_API_KEY"
+
 audio_file= open("example_video.mp4", "rb")
 transcript = openai.Audio.transcribe("whisper-1", audio_file)
 print(transcript)
