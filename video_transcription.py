7721ad4f3882140243257b347663fd4859f0157c
Molly Beach <mollyjbeach@gmail.com>
2023-04-19 10:53:06 -0700

    put videos and transcriptions in separate files

--- video_transcription.py
+++ video_transcription.py
@@ -7,18 +7,19 @@ AudioSegment.ffmpeg = "/usr/local/bin/ffmpeg"
 AudioSegment.ffprobe = "/usr/local/bin/ffprobe"
 
 # Set up OpenAI API key
-openai.api_key = "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"
+openai.api_key = "OPEN_API_KEY_HERE"
 
 # Set Paths
-audio_path = "example_video.mp4"
-audio_path2 = "part2ccla.mp4"
-output_path = "output.txt"
+video_path = "videos/example_video.mp4"
 
 # Set the maximum duration for each transcription chunk (in seconds)
 max_chunk_duration = 60
 
 # Load the audio file using pydub
-audio = AudioSegment.from_file(audio_path2, format="mp4")
+# have it look in a folder called audio
+
+audio = AudioSegment.from_file(video_path, format="mp4")
+
 
 # Calculate the total number of chunks needed
 num_chunks = math.ceil(len(audio) / (max_chunk_duration * 1000))
@@ -26,7 +27,7 @@ num_chunks = math.ceil(len(audio) / (max_chunk_duration * 1000))
 # Initialize an empty list to store the transcriptions
 transcriptions = []
 
-# Transcribe each chunk of the audio file
+# Transcribe each chunk of the audio file 
 for i in range(num_chunks):
     # Calculate the start and end times for the chunk
     start_time = i * max_chunk_duration * 1000
@@ -41,15 +42,17 @@ for i in range(num_chunks):
     # Upload the temporary WAV file to OpenAI's transcriptions API
     with open("temp.wav", "rb") as file:
         response = openai.Audio.transcribe("whisper-1", file)
+        
+    # Create a new file with the name of the input file remove first 6 characters and last 4 characters from video_path add _transcript.txt
+    output_path = "./transcriptions" + video_path[6:-4] + "_transcript.txt"
     
     # Check if the response contains the 'text' key
-    if "text" in response:
-        
+    if "text" in response: 
         # Append the transcription to the list of transcriptions
         transcriptions.append(response["text"])
          # Write the transcription to the output text file:
         open(output_path, "a").write(response["text"])
-        print("Transcription written to output.txt")
+        print("Transcription written to" + output_path)
     else:
         # If there was an error, append an empty string to the list of transcriptions
         transcriptions.append("")
